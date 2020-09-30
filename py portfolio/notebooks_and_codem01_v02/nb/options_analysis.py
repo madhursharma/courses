54d5de0976c3161229e73ea6bfa3f77c27391033
Green Sprouts <madhursharma@users.noreply.github.com>
2020-09-30 00:22:03 -0400

    add apple option pricing
    1. Add csv file
    2. Initial version of options pricing

---
+++ py portfolio/notebooks_and_codem01_v02/nb/options_analysis.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 23:26:43 2020
+
+@author: msharma
+"""
+import pandas as pd
+import scipy as sc
+import numpy as np
+
+def price_option(rows):
+    """
+
+
+    Parameters
+    ----------
+    rows : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    pandas.DataFrame
+
+    """
